d42d2b26d4b96ae9d7490c9fb92a4fbbb51a0223
Fl0-dev <florian@lamacompta.co>
2023-01-09 11:25:16 +0100

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab debitis impedit commodi perspiciatis delectus
+            quos suscipit libero ratione nisi pariatur! Dolore mollitia quos suscipit nobis praesentium? Laboriosam
+            veniam rerum maxime.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia nisi eveniet, veritatis quis porro dolorem
+            velit, quos laboriosam perferendis eaque asperiores! Perferendis sequi voluptatibus minima pariatur ratione,
+            in officiis iure!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
